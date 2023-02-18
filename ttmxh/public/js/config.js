1dda7c2a665d0b9bbd2351a24487c863f0e7fcfd
Thái Hồng Lâm <124237047+FreenBeckyisreal@users.noreply.github.com>
2023-02-18 13:55:03 +0700

    Update config.js

--- ttmxh/public/js/config.js
+++ ttmxh/public/js/config.js
@@ -1,57 +1,45 @@
 const __DATA__ = {
     'config' : {
-        'title' : 'BÙI QUANG KHẢI',
-        'favicon' : 'anhchinh.jpg',
-        'notice': 'BUI QUANG KHAI 2008'
+        'title' : 'NGUYỄN TIẾN ĐẠT',
+        'favicon' : 'https://i.imgur.com/HDyrDTe.jpg',
+        'notice': 'NGUYỄN TIẾN ĐẠT 2008'
     },
-    'avatar-image' : 'anhchinh.jpg',
-    'name' : 'Bùi Quang Khải ', 
+    'avatar-image' : 'https://i.imgur.com/HDyrDTe.jpg',
+    'name' : 'Nguyễn Tiến Đạt', 
     'is_tick' : true,
     'skills' : [
-        'DDoS Attack ',
-        'Designer',
+        'Tricker ',
+        'Blogger',
     ],
     'bio-storys' : [
         {
-            'image' : 'anhchinh.jpg',
+            'image' : 'https://i.imgur.com/HDyrDTe.jpg',
             'content' : 'LIÊN HỆ ADMIN',
-            'author' : 'Bùi Quang Khải',
+            'author' : 'Nguyễn Tiến Đạt',
         },
     ],
     'links' : [
         {
             'image' : 'wallet.png',
             'title' : 'Thông Tin Thanh Toán', 
-            'show-value' : 'Bùi Quang Khải',
+            'show-value' : 'Nguyễn Tiến Đạt ',
             'value' : 'https://xbinzh.ml/PayMent/',
             'type' : 0,
         },
         {
             'image' : 'facebook.jpg',
             'title' : 'Facebook', 
-            'show-value' : '@buiquangkhai.user4' ,
-            'value' : 'https://www.facebook.com/buiquangkhai.user4/',
+            'show-value' : '@chang.dien.2' ,
+            'value' : 'https://www.facebook.com/chang.dien.2/',
             'type' : 0,
         },
-        {
-            'image' : 'zalo.jpg',
-            'title' : 'Zalo', 
-            'show-value' : 'Bùi Quang Khải',
-            'value' : 'https://zalo.me/0352876128',
-            'type' : 0,
-        },
-        {
-            'image' : 'phone.jpg',
-            'title' : 'Hotline', 
-            'show-value' : '0352.876.128',
-            'value' : 'tel:0352876128',
-            'type' : 0,
-        },
+      
+        
         {
             'image' : 'gmail.jpg',
             'title' : 'Gmail', 
-            'show-value' : 'quangkhaicontactcopyright@gmail.com',
-            'value' : 'mailto:quangkhaicontactcopyright@gmail.com',
+            'show-value' : 'nguyentiendatchinhchu@gmail.com',
+            'value' : 'mailto:nguyentiendatchinhchu@gmail.com',
             'type' : 0,
         },
     ]
